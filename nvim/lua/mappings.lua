9d78a43577be9452972f66ba3dd90fab3568bcc0
DevCoreX <devcorexofficial@gmail.com>
2025-03-26 10:28:42 -0500

    Prettier without saving the file

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,8 +5,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- Prettier File
-map("n", "<leader>fn", ":!prettier --write %<CR>", { desc = "Format with Prettier" })
+-- Prettier Formatter
+map("n", "<leader>fn", ":%!prettier --stdin-filepath %<CR>", { desc = "Format with Prettier" })
 
 -- NeoAi Toggle Window
 vim.api.nvim_set_keymap("n", "<Space>ai", ":NeoAIToggle<CR>", { noremap = true, silent = true })

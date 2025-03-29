aa1e70643f19a2706a759514849aaa912730cbc1
DevCoreX <devcorexofficial@gmail.com>
2025-03-28 22:44:10 -0500

    Fixed some bugs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,5 @@
 
 echo -e "Updating NvChad..."
 cp -r ./nvim ~/.config
-read -p "Open Neovim? [Y/n] " open
-
-if [[ "${open}" == "y" || "${open}" == "Y" ]]; then
-  nvim
-elif [[ "${open}" == "n" || "${open}" == "n" ]]; then
-  echo -e ""
-else
-  echo -e "Canceled"
-fi
-
 echo -e "NvChad Updated!"
+echo -e "Run: nvim (recommended)"

73e972e94396b65aedf1bfff7dae12c8ccbc3402
DevCoreX <devcorexofficial@gmail.com>
2025-12-20 05:11:42 -0500

    feat(copilot): add keymaps to accept, navigate, and dismiss suggestions

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -22,3 +22,21 @@ map("n", "<leader>fs", ":%!shfmt<CR>", { desc = "Format Bash script with shfmt",
 
 -- Postgresql Formatter
 map("n", "<leader>fq", ":%!psqlformat --spaces=2 %<CR>", { desc = "Format with psqlformat" })
+
+-- GitHub Copilot
+-- Accept suggestion
+map("i", "<C-l>", function()
+  local suggestion = vim.fn["copilot#Accept"]("")
+  if suggestion ~= "" then
+    vim.api.nvim_feedkeys(suggestion, "n", true)
+  end
+end, { silent = true, desc = "Copilot accept" })
+
+-- Next suggestion
+map("i", "<C-j>", "copilot#Next()", { expr = true, silent = true, desc = "Copilot next" })
+
+-- Previous suggestion
+map("i", "<C-k>", "copilot#Previous()", { expr = true, silent = true, desc = "Copilot previous" })
+
+-- Dismiss suggestion
+map("i", "<C-h>", "copilot#Dismiss()", { expr = true, silent = true, desc = "Copilot dismiss" })
